0a4669d7e12ec654b3efe84af310168664a49719
Manfred Sorgo <110917257+sorgom@users.noreply.github.com>
2025-02-13 19:33:55 +0100

    Som devel (#5)
    * insufficiant line type
    
    * MockMacros
    
    * steps with comments; replaced printf
    
    * cleaned includes

--- TestSteps/include/TestSteps/TestSteps.h
+++ TestSteps/include/TestSteps/TestSteps.h
@@ -23,6 +23,7 @@
 #ifndef TESTSTEPS_H
 #define TESTSTEPS_H
 
+#include <TestSteps/NullStream.h>
 #include <CppUTest/UtestMacros.h>
 
 //  assign precondition / setup
@@ -31,6 +32,9 @@
 
 //  place a step
 #define STEP(n) TestSteps::step(n, __FILE__, __LINE__, __STEPPER_FUNCTION__);
+
+// place a step with comment
+#define CSTEP(n, MSG) TestSteps::cstep(n, __FILE__, __LINE__, __STEPPER_FUNCTION__) << MSG << '\n';
 
 //  begin set of steps including precondition
 #define SUBSTEPS() TestSteps::subSteps(__FILE__, __LINE__, __STEPPER_FUNCTION__);
@@ -47,6 +51,7 @@
 #define L_CHECK_FALSE(actual) L_CHECK_EQUAL(false, actual)
 //  equal size of two types / structs / classes
 #define L_CHECK_EQUAL_SIZE(T1, T2) L_CHECK_EQUAL(sizeof(T1), sizeof(T2))
+#define L_STRCMP_EQUAL(expected, actual) TestSteps::setLine(__LINE__); STRCMP_EQUAL(expected, actual);
 
 //  steps / substeps output during test
 #define TS_SHOW(level) TestSteps::show(level);
@@ -58,7 +63,7 @@ class TestSteps
 {
 public:
     typedef unsigned TestStepT;
-    typedef unsigned short TestLineT;
+    typedef unsigned TestLineT;
     typedef unsigned char StepLevelT;
 
 
@@ -69,7 +74,7 @@ public:
     static void show(StepLevelT trcLevel = 1);
 
     //  switch on tracing of test steps for all levels
-    inline static void showAll() { mShow = cNmuLevels; }
+    inline static void showAll() { show(cNmuLevels); }
 
     //  switch off tracing of test steps (default)
     inline static void hide() { mShow = 0; }
@@ -96,6 +101,14 @@ public:
 
     //  assign a step with current level / set of steps
     static void step(
+        TestStepT step,
+        const char* const file,
+        TestLineT line,
+        const char* const func,
+        bool nl = true
+    );
+
+    static std::ostream& cstep(
         TestStepT step,
         const char* const file,
         TestLineT line,
@@ -131,9 +144,9 @@ private:
 
     static const char* const c__step;
     static const char* const c__pre;
-    static const char* const c__printFile;
-    static const char* const c__printFunc;
-    static const char* const c__printLine;
+    static const char* const c__file;
+    static const char* const c__func;
+    static const char* const c__line;
     static const char* const c__ErrTestSteps;
     static const char* const c__ErrStep;
     static const char* const c__ErrSubSteps;
@@ -156,7 +169,7 @@ private:
     static bool subStepsDone();
 
     //  output of last step
-    static void out();
+    static void out(bool nl = true);
 
     //  "stack trace"
     static void trace(bool insertLine = false);
@@ -167,6 +180,8 @@ private:
 
     //  step or precondition output
     static void stepOut(TestStepT step);
+
+    static NullStream mNullStream;
 
     //  Standard 8.1.1
     TestSteps();
--- TestSteps/src/TestSteps.cpp
+++ TestSteps/src/TestSteps.cpp
@@ -1,12 +1,12 @@
 
 #include <TestSteps/TestSteps.h>
 #include <CppUTest/Utest.h>
-#include <cstdio>
-#include <cstring>
 #include <ciso646>
+#include <cstring>
+#include <iostream>
 
-using std::printf;
 using std::memset;
+using std::cout;
 
 bool TestSteps::mDone = false;
 TestSteps::StepLevelT TestSteps::mLevel = 0;
@@ -14,15 +14,17 @@ TestSteps::StepLevelT TestSteps::mShow = 0;
 
 const char* const TestSteps::c__step = "STEP(";
 const char* const TestSteps::c__pre = "PRE";
-const char* const TestSteps::c__printFile = "file: %s\n";
-const char* const TestSteps::c__printFunc = "func: %s\n";
-const char* const TestSteps::c__printLine = "line: %d\n";
+const char* const TestSteps::c__file = "file: ";
+const char* const TestSteps::c__func = "func: ";
+const char* const TestSteps::c__line = "line: ";
 const char* const TestSteps::c__ErrTestSteps = "TestSteps error";
 const char* const TestSteps::c__ErrStep = "TestSteps: STEP(n) n must be greater 0.";
 const char* const TestSteps::c__ErrSubSteps = "TestSteps: SUBSTEPS error. Did you forget ENDSTEPS?";
 const char* const TestSteps::c__ErrEndSteps = "TestSteps: ENDSTEPS error. Did you forget SUBSTEPS?";
 
 TestSteps::Trace TestSteps::mTrace[cNmuLevels] = {0};
+NullStream TestSteps::mNullStream;
+
 
 void TestSteps::clear()
 {
@@ -35,7 +37,7 @@ void TestSteps::show(const StepLevelT trcLevel)
     mShow = trcLevel;
     if (isShowing())
     {
-        printf(")\n");
+        cout << '\n';
     }
 }
 
@@ -73,7 +75,8 @@ void TestSteps::step(
     const TestStepT step,
     const char* const file,
     const TestLineT line,
-    const char* const func
+    const char* const func,
+    const bool nl
 )
 {
     UtestShell::getCurrent()->setLineNumber(line);
@@ -89,13 +92,25 @@ void TestSteps::step(
     mDone = true;
     if (mShow > mLevel)
     {
-        out();
+        out(nl);
     }
 }
 
+std::ostream& TestSteps::cstep(
+    const TestStepT cstep,
+    const char* const file,
+    const TestLineT line,
+    const char* const func
+)
+{
+    const bool show = mShow > mLevel;
+    step(cstep, file, line, func, not show);
+    return show ? std::cout << ' ' : mNullStream;
+}
+
+
 void TestSteps::setLine(const TestLineT line)
 {
-    // UtestShell::getCurrent()->setLineNumber(line);
     mTrace[mLevel].line = line;
 }
 
@@ -120,9 +135,9 @@ bool TestSteps::subStepsDone()
     return mTrace[1].step != cStepNone;
 }
 
-void TestSteps::out()
+void TestSteps::out(const bool nl)
 {
-    printf(c__step);
+    cout << c__step;
     for (StepLevelT l = 0; l < cNmuLevels; ++l)
     {
         const Trace& trc = mTrace[l];
@@ -130,19 +145,23 @@ void TestSteps::out()
         {
             if (l > 0)
             {
-                printf(".");
+                cout << '.';
             }
             stepOut(trc.step);
         }
     }
-    printf(")\n");
+    cout << ')';
+    if (nl)
+    {
+        cout << '\n';
+    }
 }
 
 void TestSteps::trace(const bool insertLine)
 {
     if (insertLine)
     {
-        printf("\n");
+        cout << '\n';
     }
 
     for (StepLevelT l = 0; l < cNmuLevels; ++l)
@@ -152,20 +171,20 @@ void TestSteps::trace(const bool insertLine)
         {
             if (l > 0)
             {
-                printf("\n");
+                cout << '\n';
             }
-            printf(c__step);
+            cout << c__step;
             stepOut(trc.step);
-            printf(")\n");
+            cout << ")\n";
             if (trc.file != 0)
             {
-                printf(c__printFile, trc.file);
+                cout << c__file << trc.file << '\n';
             }
             if (trc.func != 0)
             {
-                printf(c__printFunc, trc.func);
+                cout << c__func << trc.func << '\n';
             }
-            printf(c__printLine, trc.line);
+            cout << c__line << trc.line << '\n';
         }
     }
 }
@@ -200,10 +219,10 @@ void TestSteps::stepOut(const TestStepT step)
 {
     if (step == cStepPre)
     {
-        printf(c__pre);
+        cout << c__pre;
     }
     else
     {
-        printf("%d", step);
+        cout << step;
     }
 }
